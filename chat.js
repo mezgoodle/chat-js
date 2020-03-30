a25d62b9244b07d012fd7ab974f4c73a7baf9b89
mezgoodle <mezgoodle@gmail.com>
2020-03-30 16:35:54 +0300

    Routing for main page

--- chat.js
+++ chat.js
@@ -2,3 +2,9 @@ import express from 'express';
 let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html');
+});
